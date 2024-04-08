af4f9e9889e5a54a8cd468cec35325e156328fb1
colinbrust <colin.brust@gmail.com>
2024-04-08 15:35:12 -0600

    Simplify db compose.

--- setup/gee_to_table.R
+++ setup/gee_to_table.R
@@ -1,3 +1,5 @@
+library(magrittr)
+
 con <-
   DBI::dbConnect(RPostgres::Postgres(),
                  host = "fcfc-mesonet-db.cfc.umt.edu",
@@ -7,8 +9,28 @@ con <-
                  port = 5433
   )
 
+combine_mod13 <- function(data_dir) {
+  list.files(data_dir, full.names = T, pattern = "myd13|mod13") %>% 
+    tibble::tibble(f = .) %>% 
+    dplyr::mutate(b = basename(f) %>% 
+                    tools::file_path_sans_ext()) %>% 
+    tidyr::separate(b, c("loc", "model")) %>% 
+    dplyr::group_by(loc) %>%
+    dplyr::summarise(
+      dat = list(
+        purrr::map_df(f, readr::read_csv, show_col_types=FALSE)
+      )
+    ) %>% 
+    dplyr::mutate(out = glue::glue("./db/{loc}_mcd13.csv")) %>%
+    dplyr::rowwise() %>% 
+    dplyr::mutate(dat = list(readr::write_csv(dat, out)))
+  
+  
+}
+
+combine_mod13("./db")
 list.files("./db", full.names = T, pattern = ".csv") %>%
-  grep("preprocess.csv", ., value = T, invert = T) %>%
+  grep("preprocess.csv|myd13.csv|mod13.csv", ., value = T, invert = T) %>%
   purrr::map(function(x) {
     print(x)
     dat <- readr::read_csv(x, col_types = readr::cols(id="c")) %>%
@@ -25,12 +47,24 @@ list.files("./db", full.names = T, pattern = ".csv") %>%
           variable %in% c("ndvi", "evi") ~ value * 0.0001,
           variable %in% c("et_m16", "pet_m16") ~ value * 0.1,
           variable == "gpp" ~ value * 0.0001,
-          TRUE ~ value * 0.0001
+          stringr::str_detect(variable, "npp") ~ value * 0.0001,
+          TRUE ~ value
         )
       ) %>%
       dplyr::filter(
         variable != "ltr",
-        date < as.Date("2021-01-01"))
+        date < as.Date("2021-01-01")) %>% 
+      dplyr::group_by(year=lubridate::year(date), month=lubridate::month(date), variable, name, id) %>%
+      dplyr::summarise(
+        value = dplyr::case_when(
+          dplyr::first(variable %in% c("afg", "bgr", "evi", "ltr", "ndvi", "pfg", "shr", "tre")) ~ mean(value, na.rm = T),
+          TRUE ~ sum(value, na.rm = T)
+        ),
+        .groups = "drop"
+      ) %>%
+      dplyr::mutate(date = glue::glue("{year}-{month}-01") %>% 
+                      as.Date()) %>%
+      dplyr::select(-c(year, month))
 
 
   tab <- tools::file_path_sans_ext(x) %>%
@@ -54,15 +88,15 @@ DBI::dbDisconnect(con)
 
 
 
-# del <- tidyr::crossing(
-#   ids = c("afg", "bgr", "evi", "ltr", "pet_m16", 
-#           "pfgnpp", "shrnpp", "trenpp", "afgnpp", "et_m16",
-#           "gpp", "ndvi", "pfg", "shr", "tre"),
-#   dbs = c("historical.blm", "historical.county", "historical.huc")
-# ) %>%
-#   dplyr::mutate(
-#     delete_queries = glue::glue("DELETE from {dbs} WHERE (variable = '{ids}');")
-#   )
-# 
-# purrr::map(.x = del$delete_queries, .f = DBI::dbExecute, conn = con)
+del <- tidyr::crossing(
+  ids = c("afg", "bgr", "evi", "ltr", "pet_m16",
+          "pfgnpp", "shrnpp", "trenpp", "afgnpp", "et_m16",
+          "gpp", "ndvi", "pfg", "shr", "tre"),
+  dbs = c("historical.tribes")#  "historical.blm", "historical.county", "historical.huc")
+) %>%
+  dplyr::mutate(
+    delete_queries = glue::glue("DELETE from {dbs} WHERE (variable = '{ids}');")
+  )
+
+purrr::map(.x = del$delete_queries, .f = DBI::dbExecute, conn = con)
 
